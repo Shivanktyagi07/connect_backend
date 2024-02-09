bc97f6047de60a748e2e0746729e2efedd2ac820
Shivanktyagi07 <shivanktyagi976@gmail.com>
2024-02-09 17:47:06 +0530

    added some content

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,34 @@ app.get('/', (req, res) => {
     res.send("srever is ready")
 })
 
+//List of 5 jokes:
+app.get('/jokes', (req, res)=>{
+   const jokes = [
+    {
+        id: 1,
+        title: 'A joke',
+        content: 'this is a joke'
+    },
+    {
+        id: 2,
+        title: 'Another joke',
+        content: 'this is another joke'
+    },
+    {
+        id: 3,
+        title:'third joke',
+        content:'this is third joke'
+    },
+    {
+        id: 4,
+       title: 'fourth joke',
+       content: 'this of fouth joke'
+     }
+   ];
+   res.send(jokes)
+});
+
 app.listen(port, () =>{
     console.log(`server is running at${port}`);
 })
+
