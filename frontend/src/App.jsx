72e3daeba53b2aa0408f4bc24a07590639a2fd8b
Shivanktyagi07 <shivanktyagi976@gmail.com>
2024-02-10 12:09:47 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+  // const [jokes, setJokes] = useState([]);
+
+  return (
+   <>
+     <h1>fullstack</h1>
+     {/* <p>JOKES : {jokes.length}</p> */}
+   </>
+  )
+}
+
+export default App
